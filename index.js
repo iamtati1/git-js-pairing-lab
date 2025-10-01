35dd2f711aa99e360d419d0424f09be84ef44aeb
iamtati1 <tatianabarmer@gmail.com>
2025-10-01 12:37:51 -0400

    Completed my first function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //Code your solutions in this file
+//This is my 1st function - Tatiana
+const fiveToOneHundred = () => {
+    for (let i = 5; i <= 100; i++) {
+        console.log(i);
+    }
+}
+fiveToOneHundred();
